1b41f8528661dfbe029dcb68cea26f2435378a86
Lucabhm <lbohm@student.42heilbronn.de>
2026-01-20 16:00:12 +0100

    added Change Language Card

---
+++ app/src/main/java/com/example/ft_hangouts/ui/components/LanguageCard.kt
@@ -0,0 +1,61 @@
+package com.example.ft_hangouts.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LanguageCard(modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedLang by remember { mutableStateOf("English") }
+    val languages = listOf("German", "English")
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .clickable(onClick = {expanded = true})
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text("Change Language", fontSize = 25.sp)
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = modifier.fillMaxWidth()
+        ) {
+            languages.forEach { language ->
+                DropdownMenuItem(
+                    text = { Text(language) },
+                    onClick = { selectedLang = language },
+                    trailingIcon = {if (selectedLang == language) { Icon(Icons.Default.Done, contentDescription = null) }}
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/ft_hangouts/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/ft_hangouts/ui/screens/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.ft_hangouts.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,15 +12,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.ft_hangouts.ui.components.HeaderColorCard
+import com.example.ft_hangouts.ui.components.LanguageCard
 
 @Preview
 @Composable
 fun SettingsScreen(modifier: Modifier = Modifier, navController: NavController) {
     val scrollState = rememberScrollState()
-    Column(modifier = modifier
-        .fillMaxSize()
-        .padding(5.dp)
-        .verticalScroll(scrollState)) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(5.dp)
+            .verticalScroll(scrollState), verticalArrangement = Arrangement.spacedBy(5.dp)
+    ) {
         HeaderColorCard(modifier, navController)
+        LanguageCard(modifier)
     }
 }
