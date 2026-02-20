f9210eab23f157d3f88b0bb97ad75aeea5424af5
Lucabhm <lbohm@student.42heilbronn.de>
2026-02-20 10:40:04 +0100

    added SMS receiver in Manifest und MainActivity

--- app/src/main/java/com/example/ft_hangouts/MainActivity.kt
+++ app/src/main/java/com/example/ft_hangouts/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.ft_hangouts
 
-import android.content.BroadcastReceiver
-import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.Modifier
-import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
 import com.example.ft_hangouts.data.local.AppContainer
 import com.example.ft_hangouts.ui.addContact.AddContactViewModel
 import com.example.ft_hangouts.ui.call.CallViewModel
@@ -16,29 +14,42 @@ import com.example.ft_hangouts.ui.message.SmsReceiver
 import com.example.ft_hangouts.ui.navigation.AppNavigation
 import com.example.ft_hangouts.ui.navigation.NavViewModel
 import com.example.ft_hangouts.ui.theme.Ft_hangoutsTheme
+import java.util.jar.Manifest
 
 class MainActivity : ComponentActivity() {
     private val _container = AppContainer(this)
-    private val navViewModel = NavViewModel()
-    private val chatViewModel = ChatsViewModel(_container.contactRepo)
-    private val callViewModel = CallViewModel(_container.callRepo)
-    private val messageViewModel = MessageViewModel(_container.messageRepo, _container.smsRepository)
-    private val addContactViewModel = AddContactViewModel(_container.contactRepo)
+    private val _navViewModel = NavViewModel()
+    private val _chatViewModel = ChatsViewModel(_container.contactRepo)
+    private val _callViewModel = CallViewModel(_container.callRepo)
+    private val _messageViewModel =
+        MessageViewModel(_container.messageRepo, _container.smsRepository)
+    private val _addContactViewModel = AddContactViewModel(_container.contactRepo)
 
-    private val smsReceiver = SmsReceiver()
+    private val _smsReceiver = SmsReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.SEND_SMS,
+                android.Manifest.permission.RECEIVE_SMS,
+                android.Manifest.permission.READ_SMS,
+                android.Manifest.permission.BROADCAST_SMS
+            ),
+            1
+        )
 
         setContent {
             Ft_hangoutsTheme {
                 AppNavigation(
                     modifier = Modifier,
-                    navViewModel,
-                    chatViewModel,
-                    messageViewModel,
-                    callViewModel,
-                    addContactViewModel
+                    _navViewModel,
+                    _chatViewModel,
+                    _messageViewModel,
+                    _callViewModel,
+                    _addContactViewModel
                 )
             }
         }
